c5c0712690cffe84e59adb40e6d7c01365045d36
Matt Solt <mattsolt@gmail.com>
2020-07-11 16:45:43 -0400

    add Store with initial quiz questions

---
+++ index.js
@@ -0,0 +1,113 @@
+var Store = {
+  questions: [
+    //1
+    {
+      question: "How old is the art of barbecue?",
+      options: [
+        "a. Invented in the 1950's",
+        "b. Invented by Abraham Lincoln in 1830",
+        "c. Has been around ever since meat was cooked over an open flame",
+        "d. Since Roman times",
+      ],
+      answer: 2,
+    },
+    //2
+    {
+      question: "What are the major barbecue styles of the United States?",
+      options: [
+        "a. North, South, East, West",
+        "b. Maine style and Oregon Style",
+        "c. New York, Deep Dish, and Detroit",
+        "d. Texas, Carolina, Kansas City and Alabama",
+      ],
+      answer: 3,
+    },
+    //3
+    {
+      question: "Which style is know for using beef as the main meat source?",
+      options: [
+        "a.Carolina",
+        "b.Alabama",
+        "c.Texas",
+        "d. Kansas City"],
+      answer: 2,
+    },
+    //4
+    {
+      question: "Which region is famous for having 3 different styles of barbecue?",
+      options: [
+        "a.Alabama",
+        "b.Carolina",
+        "c.Texas",
+        "d.Kansas City"
+      ],
+      answer: 1,
+    },
+    //5
+    {
+      question: "Which region is considered the crossroads of bbq?",
+      options: [
+        "a. Texas",
+        "b. Alabama",
+        "c. Kansas City",
+        "d. Florida"],
+      answer: 2,
+    },
+    //6
+    {
+      question: "Which region is famous for having white barbecue sauce?",
+      options: [
+        "a. Alabama",
+        "b. Delaware",
+        "c. Pacific Northwest",
+        "d. Homestyle",
+      ],
+      answer: 0,
+    },
+    //7
+    {
+      question: "How is barbecue typically cooked?",
+      options: [
+        "a. Hot and Fast",
+        "b. COld and Quick",
+        "c. Microwave",
+        "d. Low and Slow",
+      ],
+      answer: 3,
+    },
+    //8
+    {
+      question: "What temperature is most barbecue cooked to?",
+      options: [
+        "a. 145°F",
+        "b. 32°F",
+        "c. 203°C",
+        "d. 203°F"],
+      answer: 3,
+    },
+    //9
+    {
+      question: "What type of heat is used for cooking barbecue?",
+      options: [
+        "a. Searing",
+        "b. Indirect heat",
+        "c. Sous vide",
+        "d. Flash fry",
+      ],
+      answer: 1,
+    },
+    //10
+    {
+      question: "WHich tool is most important for cooking barbecue?",
+      options: [
+        "a.Kitchen sink",
+        "b. Grill/smoker",
+        "c. Knife",
+        "d. Spatula"],
+      answer: 1,
+    },
+  ],
+  correctAnswers: 0,
+  questionIndex: 0,
+  quizStarted: false
+};
